23690c824f4f93ccedd349d2ba632509f61d9f34
Phillip Taylor <phil.taylor.sj@gmail.com>
2024-06-10 17:37:11 +0100

    Created functions for date and id validations.

--- src/validation.py
+++ src/validation.py
@@ -11,12 +11,28 @@ def check_date_is_valid(date: str) -> bool:
     '''
     date_from = datetime(1, 1, 1)
     if not isinstance(date, str):
-        raise TypeError('date_from parameter must be of type string.')
+        raise TypeError('Parameter (date_from) must be of type string.')
     try:
         date_from = datetime.strptime(date, '%Y-%m-%d')
     except ValueError:
-        raise ValueError('date_from parameter must be formatted as %Y-%m-%d.')
+        raise ValueError(
+            'Parameter (date_from) must be formatted as %Y-%m-%d.'
+            )
 
     if date_from >= datetime.now():
-        raise ValueError('date_from parameter must be before current date.')
+        raise ValueError(
+            'Parameter (date_from) must be before current date.'
+            )
+    return True
+
+
+def check_id_string_is_valid(id: str, param_name: str) -> bool:
+    if not isinstance(id, str):
+        raise TypeError(f'Parameter ({param_name}) must be of type string.')
+    if len(id) == 0:
+        raise ValueError(f'Parameter ({param_name}) cannot be '
+                         'an empty string.')
+    if len(id.strip()) == 0:
+        raise ValueError(f'Parameter ({param_name}) cannot contain ' +
+                         'only whitespace.')
     return True
--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,4 +1,7 @@
-from src.validation import check_date_is_valid
+import re
+from src.validation import (
+    check_date_is_valid, check_id_string_is_valid
+    )
 import pytest
 
 
@@ -12,23 +15,66 @@ class TestcCheckDateIsValid():
     def test_raises_error_for_none_string_value(self):
         with pytest.raises(TypeError) as exec:
             check_date_is_valid(14)
-        assert exec.match('date_from parameter must be of type string.')
+        assert exec.match(re.escape(
+            'Parameter (date_from) must be of type string.'
+            ))
         with pytest.raises(TypeError) as exec:
             check_date_is_valid([])
-        assert exec.match('date_from parameter must be of type string.')
+        assert exec.match(re.escape(
+            'Parameter (date_from) must be of type string.'
+            ))
 
     def test_raises_error_for_invalid_date_format(self):
         with pytest.raises(ValueError) as exec:
             check_date_is_valid('Hello World')
-        assert exec.match('date_from parameter must be formatted as %Y-%m-%d.')
+        assert exec.match(re.escape(
+            'Parameter (date_from) must be formatted as %Y-%m-%d.'
+                                    ))
         with pytest.raises(ValueError) as exec:
             check_date_is_valid('0000-00-00')
-        assert exec.match('date_from parameter must be formatted as %Y-%m-%d.')
+        assert exec.match(re.escape(
+            'Parameter (date_from) must be formatted as %Y-%m-%d.'
+                                    ))
         with pytest.raises(ValueError) as exec:
             check_date_is_valid('')
-        assert exec.match('date_from parameter must be formatted as %Y-%m-%d.')
+        assert exec.match(re.escape(
+            'Parameter (date_from) must be formatted as %Y-%m-%d.'))
 
     def test_raises_error_for_invalid_date_value(self):
         with pytest.raises(ValueError) as exec:
             check_date_is_valid('3020-01-01')
-        assert exec.match('date_from parameter must be before current date.')
+        assert exec.match(re.escape(
+            'Parameter (date_from) must be before current date.'
+            ))
+
+
+class TestCheckIdStringIsValid:
+
+    def test_returns_true_for_valid_id(self):
+        assert check_id_string_is_valid('Hello World', 'test_id')
+        assert check_id_string_is_valid('  1234-hello', 'test_id')
+        assert check_id_string_is_valid('  1234-world   ', 'test_id')
+
+    def test_raises_error_for_invalid_type(self):
+        with pytest.raises(TypeError) as exec:
+            check_id_string_is_valid(20, 'test_id')
+        assert exec.match(re.escape(
+            'Parameter (test_id) must be of type string.'
+            ))
+        with pytest.raises(TypeError) as exec:
+            check_id_string_is_valid([], 'test_id')
+        assert exec.match(re.escape(
+            'Parameter (test_id) must be of type string.'
+            ))
+
+    def test_raises_error_for_empty_id_string(self):
+        with pytest.raises(ValueError) as exec:
+            check_id_string_is_valid('', 'test_id')
+        assert exec.match(re.escape(
+            'Parameter (test_id) cannot be an empty string.'
+            ))
+        with pytest.raises(ValueError) as exec:
+            check_id_string_is_valid('   ', 'test_id')
+        assert exec.match(re.escape(
+            'Parameter (test_id) cannot contain only whitespace.'
+        ))
