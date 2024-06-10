5f33de0a81c6ed18eecba89a0e0359fdc31aa25f
Phillip Taylor <phil.taylor.sj@gmail.com>
2024-06-10 18:03:31 +0100

    Added docstrings to validation functions.

--- src/validation.py
+++ src/validation.py
@@ -3,11 +3,21 @@ from datetime import datetime
 
 def check_date_is_valid(date: str) -> bool:
     '''
-    Args:
-        date: string value of date parameter to validate.
+    Validate that a given date string meets specific criteria.
+
+    This function checks whether the provided `date` parameter is a string
+    formatted as `YYYY-MM-DD` and represents a date that is before the current date.
+    If the `date` does not meet these criteria, an appropriate exception is raised.
+
+    Parameters:
+        date (str): The date string to be validated, formatted as `YYYY-MM-DD`.
 
     Returns:
-        True if no exception is thrown.
+        bool: True if the `date` is valid.
+
+    Raises:
+        TypeError: If `date` is not of type `str`.
+        ValueError: If `date` is not formatted as `YYYY-MM-DD` or if it is not before the current date.
     '''
     date_from = datetime(1, 1, 1)
     if not isinstance(date, str):
@@ -27,6 +37,24 @@ def check_date_is_valid(date: str) -> bool:
 
 
 def check_id_string_is_valid(id: str, param_name: str) -> bool:
+    '''
+    Validate that a given ID string meets specific criteria.
+
+    This function checks whether the provided `id` parameter is a non-empty
+    string that does not consist solely of whitespace characters. If the `id`
+    does not meet these criteria, an appropriate exception is raised.
+
+    Parameters:
+        id (str): The string to be validated.
+        param_name (str): The name of the parameter, used in error messages for clarity.
+
+    Returns:
+        bool: True if the `id` is valid.
+
+    Raises:
+        TypeError: If `id` is not of type `str`.
+        ValueError: If `id` is an empty string or contains only whitespace.
+    '''
     if not isinstance(id, str):
         raise TypeError(f'Parameter ({param_name}) must be of type string.')
     if len(id) == 0:
